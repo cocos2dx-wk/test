e2f4d8884e4194243431d1f9bd91ee20d5fba5d5
wangkai <wangkai@che001.com>
2016-08-17 11:02:01 +0800

    测试新建工程

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by wangkai on 16/8/17.
+//  Copyright © 2016年 wangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by wangkai on 16/8/17.
+//  Copyright © 2016年 wangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
